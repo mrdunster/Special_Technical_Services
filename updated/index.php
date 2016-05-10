ac805715bc3d40dc29b17c39e289341a27b680da
Ivan Melnikov <ivanempire@gmail.com>
2016-05-10 10:00:53 +0300

    Working mini section

--- updated/index.php
+++ updated/index.php
@@ -32,31 +32,34 @@
 	</div>
 	<!--END Banner Picture-->
 
+
 	<!--Mini Content-->
-	<ul class="uk-grid uk-grid-width-1-1 uk-grid-width-medium-1-2 uk-grid-width-large-1-6 uk-container-center uk-margin mini_content_info" data-uk-margin>
-			<li class="uk-visible-large"></li>
-			<li>
-				<h3><img src="assets/img/energy.png">Static Control</h3>
-		    	<p>STS offers many options to help protect personal equipment, material and plant operations from catastrophic damage. </p>
-		    	<a href="units.php">Read More <i class="uk-icon-caret-right"></i></a>
-			</li>
-			<li>
-				<h3><img src="assets/img/wrench.png">Replacement Parts</h3>
-		    	<p>Did something break? Check out our wide range of replacement parts available.</p>
-		    	<a href="replacement.php">Read More <i class="uk-icon-caret-right"></i></a>
-			</li>
-			<li>
-				<h3><img src="assets/img/truck.png">Current Applications</h3>
-		    	<p>Our products can be found on mobile vehicles, machinery, receiving containers, and marine vessels.</p>
-		    	<a href="applications.php">Read More <i class="uk-icon-caret-right"></i></a>
-			</li>
-			<li>
-				<h3><img src="assets/img/purchase.png">Purchasing Product</h3>
-		    	<p>Already know what you need? Check out our extensive ordering selections available.</p>
-		    	<a href="ordering_guide.php">Read More <i class="uk-icon-caret-right"></i></a><!--Add RFQ link here eventually -->
-			</li>
-	</ul>
-	<!--END Mini Content-->
+	<section id="miniLinks" class="uk-width-3-4 uk-container-center">
+		<article class="uk-width-1-4">
+		<h3><img class="uk-res"src="assets/img/energy.png">Static Control</h3>
+		<p>STS offers many options to help protect personal equipment, material and plant operations from catastrophic damage. </p>
+		<a href="units.php">Read More <i class="uk-icon-caret-right"></i></a>
+		</article>
+
+		<article class="uk-width-1-4">
+		<h3><img src="assets/img/wrench.png">Replacement Parts</h3>
+		<p>Did something break? Check out our wide range of replacement parts available.</p>
+		<a href="replacement.php">Read More <i class="uk-icon-caret-right"></i></a>
+		</article>
+
+		<article class="uk-width-1-4">
+		<h3><img src="assets/img/truck.png">Current Applications</h3>
+		<p>Our products can be found on mobile vehicles, machinery, receiving containers, and marine vessels.</p>
+		<a href="applications.php">Read More <i class="uk-icon-caret-right"></i></a>
+		</article>
+
+		<article class="uk-width-1-4">
+		<h3><img src="assets/img/purchase.png">Purchasing Product</h3>
+		<p>Already know what you need? Check out our extensive ordering selections available.</p>
+		<a href="ordering_guide.php">Read More <i class="uk-icon-caret-right"></i></a><!--Add RFQ link here eventually -->
+		</article>
+
+	</section>
 
 <?php include 'footer.php';?>
 
